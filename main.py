e4388bcb65421eba159d3ba1f581301c8dbc86ea
nphuc0311 <ndphuc3112@gmail.com>
2023-05-19 23:11:36 +0700

    Update commit

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ if __name__ == "__main__":
     # Loading face detector
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor("pretrain/shape_predictor_68_face_landmarks.dat")
-    gif_to_numpy("gif/glow_mini.gif")
-
+    # gif_to_numpy("gif/glow_mini.gif")
+    frame_count = 0
     while 1:
         ret, frame = cap.read()
         if ret:
-        
-            frame = phuc_effect(detector, predictor, frame)
-        #   frame = phat_effect(detector, predictor, frame)
-        #   frame = nam_effect(detector, predictor, frame)
+
+            frame = phuc_effect(detector, predictor, frame, frame_count)
+            frame = phat_effect(detector, predictor, frame, frame_count)
+            # frame = nam_effect(detector, predictor, frame)
+
+            frame_count += 1
 
             cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
--- ultis.py
+++ ultis.py
@@ -5,41 +5,48 @@ import numpy as np
 import cv2
 import dlib
 from math import hypot
+import imageio
 
 def gif_to_numpy(image):
-    frames, exts, image_specs = gif2numpy.convert(image)
+    frames, _, _ = gif2numpy.convert(image)
     sprites=np.array(frames)
-
     np.save("gif/effect.npy", sprites)
+
+    frames = imageio.mimread(imageio.core.urlopen("https://i.gifer.com/Dzpn.gif").read(), '.gif')
+    sprites = [frame[...,0:3] for frame in frames]
+    sprites=np.array(sprites)
+    # print(len(sprites))
+    np.save("gif/effect_2.npy", sprites)
 
 
 def merge(frame, hair, mask, centre):
 
-  frows, fcols, _ = frame.shape
-  hrows, hcols, _ = hair.shape
-  
-  x, y = (centre[0] - hcols//2, centre[1] -  hrows//2)
- 
-  frame_roi = frame[ y:y+hrows, x:x+hcols ,... ]
+    frows, fcols, _ = frame.shape
+    hrows, hcols, _ = hair.shape
+    
+    x, y = (centre[0] - hcols//2, centre[1] - hrows//2)
+    
+    frame_roi = frame[ y:y+hrows, x:x+hcols ,... ]
 
-  # Check bounds and overalay hair
-  if frame_roi.shape == hair.shape:  
-    np.copyto(frame_roi, hair,where=mask[...,np.newaxis]>0)
+    # Check bounds and overalay hair
+    if frame_roi.shape == hair.shape:  
+        np.copyto(frame_roi, hair,where=mask[...,np.newaxis]>0)
+
 
 # Add glow overlay image
 def add_glow(frame, glow, origin):
     
-   frows, fcols, _ = frame.shape
-   grows, gcols, _ = glow.shape
-   
-   x, y = origin
+    frows, fcols, _ = frame.shape
+    grows, gcols, _ = glow.shape
+    
+    x, y = origin
 
-   frame_roi = frame[ y:y+grows, x:x+gcols ,... ]
-   
-   # Check bounds and overlay aura glow
-   if frame_roi.shape ==  glow.shape:
-    glow_roi = cv2.addWeighted(frame_roi,1.0, glow,0.95,0)
-    np.copyto(frame_roi, glow_roi)
+    frame_roi = frame[ y:y+grows, x:x+gcols ,... ]
+    
+    # Check bounds and overlay aura glow
+    if frame_roi.shape ==  glow.shape:
+        glow_roi = cv2.addWeighted(frame_roi,1.0, glow,0.95,0)
+        np.copyto(frame_roi, glow_roi)
 
 # Check if mouth is open
 def mouth_open(landmarks):
@@ -63,20 +70,19 @@ def mouth_open(landmarks):
        # mouth is closed
        return False
 
-def phuc_effect(detector, predictor, frame):
+def phuc_effect(detector, predictor, frame, frame_count):
     # Load custom hair image with alpha mask
     hair_image = cv2.imread("images/ssj_hair.png")
     hair_mask = cv2.imread("images/ssj_hair.png", cv2.IMREAD_UNCHANGED)[...,3]
 
     # Load golden aura sprite images (animation)
     glow_imgs = np.load('gif/effect.npy')
+    # print(len(glow_imgs))
+    glow_imgs_2 = np.load('gif/effect_2.npy')
 
     # Load black hair image with alpha mask
     black_hair = cv2.imread("images/black_hair.png")
     black_hair_mask = cv2.imread("images/black_hair.png", cv2.IMREAD_UNCHANGED)[...,3]
-
-    frame_count = 1
-
 
     # Convert frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -99,20 +105,20 @@ def phuc_effect(detector, predictor, frame):
             hair_im = hair_image
             hair_mk = hair_mask
             hwidth_multiplier = 2
-            htop_multiplier = 2
+            htop_multiplier = 100
             ssj = True
 
         else:
             hair_im = black_hair
             hair_mk = black_hair_mask
             hwidth_multiplier = 2.6
-            htop_multiplier = 3
+            htop_multiplier = 100
             ssj = False           
 
         # Configure hair width and height
         hair_width = int(hypot(left_face[0] - right_face[0],
                         left_face[1] - right_face[1])*hwidth_multiplier)
-        hair_height = int(hair_width * 0.9)
+        hair_height = int(hair_width)
 
         # Resize hair and mask
         hair =  cv2.resize(hair_im, (hair_width, hair_height))
@@ -120,12 +126,14 @@ def phuc_effect(detector, predictor, frame):
 
         # Compute hair location
         hair_x = landmarks.part(27).x
-        hair_y = int(landmarks.part(27).y  + (landmarks.part(27).y - landmarks.part(30).y )*htop_multiplier)
+        hair_y = int(landmarks.part(27).y - htop_multiplier)
 
         merge(frame, hair, mask, (hair_x, hair_y))
+        # add_glow(frame, glow, (glow_x_left , glow_y_top))
 
         # Resize the aura image frame
-        glow =  cv2.resize(glow_imgs[frame_count%4], (int(hair_width*2.4), int(hair_width*3.4) ))
+        glow =  cv2.resize(glow_imgs[frame_count%int(len(glow_imgs))], (int(hair_width*2.4), int(hair_width*3.4) ))
+        glow_2 = cv2.resize(glow_imgs_2[frame_count%int(len(glow_imgs_2))], (int(hair_width*2.4), int(hair_width*3.4) ))
 
         # Compute the hair location
         glow_x_left = landmarks.part(27).x - int(hair_width * 1.2)
@@ -133,22 +141,73 @@ def phuc_effect(detector, predictor, frame):
         
         # Crop the aura image based on height 
         glow = glow[0:frame.shape[0]-glow_y_top, ...]
+        glow_2 = glow_2[0:frame.shape[0]-glow_y_top, ...]
         if ssj:
             add_glow(frame, glow, (glow_x_left , glow_y_top))
+            add_glow(frame, glow_2, (glow_x_left , glow_y_top))
+
 
     return frame
 
 
-def phat_effect(detector, predictor, frame):
+def phat_effect(detector, predictor, frame, frame_count):
    
-    # Write your code here
+    # Load custom hair image with alpha mask
+    eyes_image = cv2.imread("images/rinnergan.png")
+    eyes_mask = cv2.imread("images/rinnergan.png", cv2.IMREAD_UNCHANGED)[...,3]
 
-   return frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces from the grayscale image
+    faces = detector(gray)
+
+    if len(faces) == 1:
+        landmarks = predictor(gray, faces[0])
+
+        # Left eyes
+        left_point = (landmarks.part(37).x, landmarks.part(37).y)
+        right_point = (landmarks.part(40).x, landmarks.part(40).y)
+
+        eyes_width = abs(left_point[0]-right_point[0])*2
+        eyes_height = int(eyes_width*frame.shape[0]/frame.shape[1])
+
+        eyes = cv2.resize(eyes_image, (eyes_width, eyes_height))
+        mask =  cv2.resize(eyes_mask, (eyes_width, eyes_height), interpolation = cv2.INTER_NEAREST)
+
+        eyes_x = int(landmarks.part(37).x + abs((landmarks.part(38).x - landmarks.part(37).x)//2))
+        eyes_y = abs(landmarks.part(38).y)
+
+        merge(frame, eyes, mask, (eyes_x, eyes_y))
+
+        # Right eyes
+        left_point = (landmarks.part(43).x, landmarks.part(43).y)
+        right_point = (landmarks.part(46).x, landmarks.part(46).y)
+
+        eyes_width = abs(left_point[0]-right_point[0])*2
+        eyes_height = int(eyes_width*frame.shape[0]/frame.shape[1])
+
+        eyes = cv2.resize(eyes_image, (eyes_width, eyes_height))
+        mask =  cv2.resize(eyes_mask, (eyes_width, eyes_height), interpolation = cv2.INTER_NEAREST)
+
+        eyes_x = int(landmarks.part(43).x + abs((landmarks.part(43).x - landmarks.part(44).x)//2))
+        eyes_y = abs(landmarks.part(44).y)
+
+        # print(eyes_width, eyes_height)
+
+        merge(frame, eyes, mask, (eyes_x, eyes_y))
+
+
+    return frame
+
+    
+
+
+    return frame
 
 
 def nam_effect(detector, predictor, frame):
    
-    # Write your code here
+    # Write your code heref
 
    return frame
 
